a4e5ebabc0b16300c3784926777b52492c32c99d
Golovin Pavel <gopavel0@gmail.com>
2018-02-20 01:14:45 +0300

    HW 1 commit 2

--- java/ru/ifmo/rain/golovin/walk/FileVisitorHash.java
+++ java/ru/ifmo/rain/golovin/walk/FileVisitorHash.java
@@ -1,17 +1,69 @@
 package ru.ifmo.rain.golovin.walk;
 
 import java.io.BufferedWriter;
-import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class FileVisitorHash extends SimpleFileVisitor<Path> {
     BufferedWriter writer;
+    int sizeBuffer = 1024;
+    byte[] buffer  = new byte[sizeBuffer];
 
-    public FileVisitorHash(String pathnameStart, BufferedWriter bufferedWriter) {
+
+    public FileVisitorHash(BufferedWriter bufferedWriter) {
         writer = bufferedWriter;
     }
-    //TODO
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        int acc = 0x811c9dc5;
+        try (FileInputStream reader = new FileInputStream(file.toString())) {
+            for (int cntReaded = reader.read(buffer, 0, sizeBuffer); cntReaded > 0; cntReaded = reader.read(buffer, 0, sizeBuffer)) {
+                for (int i = 0; i < cntReaded; ++i) {
+                    acc = (acc * 0x01000193) ^ (buffer[i] & 0xff);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Path: \"" + file.toString() + "\" is not found");
+            acc = 0;
+        } catch (IOException ignore) {
+            acc = 0;
+        } finally {
+            try {
+                writer.write(String.format("%08X", acc).toLowerCase() + " " + file.toString());
+                writer.newLine();
+            } catch (IOException e) {
+                System.out.println("Can't write in output file"
+                        + "\"" + String.format("%08X", acc).toLowerCase() + " " + file.toString() + "\"");
+            }
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) {
+        try {
+            writer.write(String.format("%08X", 0).toLowerCase() + " " + file.toString());
+            writer.newLine();
+            System.out.println("Open or read file error");
+        } catch (IOException ignore) {
+
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+        if (exc == null)
+            return FileVisitResult.CONTINUE;
+        else {
+            System.out.println("I\\O error with directory");
+            return FileVisitResult.CONTINUE;
+        }
+    }
 }
--- java/ru/ifmo/rain/golovin/walk/RecursiveWalk.java
+++ java/ru/ifmo/rain/golovin/walk/RecursiveWalk.java
@@ -8,23 +8,27 @@ import java.io.InputStreamReader;
 import java.io.FileInputStream;
 import java.io.OutputStreamWriter;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class RecursiveWalk {
     public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0]),  "UTF8"))) {
-            try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(args[1]), "UTF8"))) {
-                for (String pathname = reader.readLine(); pathname != null; pathname = reader.readLine()) {
-                    Files.walkFileTree(Paths.get(pathname), new FileVisitorHash(pathname, writer));
+
+        if (args.length < 2) {
+            System.out.println("Too few args");
+        } else if (args.length > 2) {
+            System.out.println("Too many args");
+        } else {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0]), "UTF8"))) {
+                try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(args[1]), "UTF8"))) {
+                    for (String pathname = reader.readLine(); pathname != null; pathname = reader.readLine()) {
+                        Files.walkFileTree(Paths.get(pathname), new FileVisitorHash(writer));
+                    }
+                } catch (IOException ignore) {
+                    System.out.println("Output file open error");
                 }
+            } catch (IOException ignore) {
+                System.out.println("Input file open error");
             }
-            catch (IOException ignore) {
-                System.out.print("Output file open error");
-            }
-        }
-        catch (IOException ignore) {
-            System.out.println("Input file open error");
         }
     }
 }
