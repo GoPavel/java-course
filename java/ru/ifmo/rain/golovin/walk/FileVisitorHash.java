b1617d1f9993237b02562ab2174811c4c08716c7
Golovin Pavel <gopavel0@gmail.com>
2018-02-26 23:56:55 +0300

    HW 1 commit 4

--- java/ru/ifmo/rain/golovin/walk/FileVisitorHash.java
+++ java/ru/ifmo/rain/golovin/walk/FileVisitorHash.java
@@ -11,58 +11,82 @@ import java.nio.file.attribute.BasicFileAttributes;
 
 public class FileVisitorHash extends SimpleFileVisitor<Path> {
     BufferedWriter writer;
+    String namePathOfWriter;
     int sizeBuffer = 1024;
     byte[] buffer  = new byte[sizeBuffer];
 
 
-    public FileVisitorHash(BufferedWriter bufferedWriter) {
-        writer = bufferedWriter;
+    public FileVisitorHash(BufferedWriter writer, String namePathOfWriter) {
+        this.writer = writer;
+        this.namePathOfWriter = namePathOfWriter;
+    }
+
+    private void writeHash(int hash, Path path) {
+        String text = String.format("%08X", hash).toLowerCase() + " " + path.toString();
+        try {
+            writer.write(text);
+            writer.newLine();
+        } catch (IOException e) {
+            System.out.println("I/O exception during write to output file. message: " + "\"" + e.getMessage() + "\"");
+            System.out.println("Unwritten text: " + "\"" + text + "\"");
+        }
     }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
         int acc = 0x811c9dc5;
         try (FileInputStream reader = new FileInputStream(file.toString())) {
-            for (int cntReaded = reader.read(buffer, 0, sizeBuffer); cntReaded > 0; cntReaded = reader.read(buffer, 0, sizeBuffer)) {
-                for (int i = 0; i < cntReaded; ++i) {
-                    acc = (acc * 0x01000193) ^ (buffer[i] & 0xff);
+            try {
+                for (int cntReaded = reader.read(buffer, 0, sizeBuffer); cntReaded > 0; cntReaded = reader.read(buffer, 0, sizeBuffer)) {
+                    for (int i = 0; i < cntReaded; ++i) {
+                        acc = (acc * 0x01000193) ^ (buffer[i] & 0xff);
+                    }
                 }
+            } catch (IOException e) {
+                System.out.println("I/O exception during read file when walk in path " + "\"" + namePathOfWriter + "\"");
+                System.out.println("Exception message: " + "\"" + e.getMessage() + "\"");
+                acc = 0;
             }
         } catch (FileNotFoundException e) {
-            System.out.println("Path: \"" + file.toString() + "\" is not found");
+            System.out.println("File not found exception during open file when walk in path " + "\"" + namePathOfWriter + "\"");
+            System.out.println("Exception message: " + "\"" + e.getMessage() + "\"");
             acc = 0;
-        } catch (IOException ignore) {
+        } catch (SecurityException e) {
+            System.out.println("Security exception during open file when walk in path " + "\"" + namePathOfWriter + "\"");
+            System.out.println("Exception message: " + "\"" + e.getMessage() + "\"");
+            acc = 0;
+        } catch (IOException e) {
+            System.out.println("I/O exception during close file when walk in path " + "\"" + namePathOfWriter + "\"");
+            System.out.println("Exception message: " + "\"" + e.getMessage() + "\"");
             acc = 0;
         } finally {
-            try {
-                writer.write(String.format("%08X", acc).toLowerCase() + " " + file.toString());
-                writer.newLine();
-            } catch (IOException e) {
-                System.out.println("Can't write in output file"
-                        + "\"" + String.format("%08X", acc).toLowerCase() + " " + file.toString() + "\"");
-            }
+            writeHash(acc, file);
         }
         return FileVisitResult.CONTINUE;
     }
 
     @Override
     public FileVisitResult visitFileFailed(Path file, IOException exc) {
-        try {
-            writer.write(String.format("%08X", 0).toLowerCase() + " " + file.toString());
-            writer.newLine();
-            System.out.println("Open or read error with file \"" + file.toString() + "\"");
-        } catch (IOException ignore) {
-
-        }
+        writeHash(0, file);
+        System.out.println("File " + "\"" + file.toString() + "\" " + "couldn't be visit with I/O exception " + "\"" + exc.getClass().getCanonicalName() + "\" " +
+            "when walk in path " + "\"" + namePathOfWriter + "\".");
+        System.out.println("Exception message: " + exc.getMessage());
         return FileVisitResult.CONTINUE;
     }
+
+    /*
+    * Note: Как можно обрабытывать исключения, которые нам приходят как параметры? Нужно ли пытаться найти динамический тип?
+    * Ведь специфицировать статический исключения при передачи его как параметр невозможно.
+     */
 
     @Override
     public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
         if (exc == null)
             return FileVisitResult.CONTINUE;
         else {
-            System.out.println("I\\O error with directory");
+            System.out.println("I/O exception " + "\"" + exc.getClass().getCanonicalName() + "\" " +
+                                "when walk in path " + "\"" + namePathOfWriter + "\" and iteration completed prematurely.");
+            System.out.println("Exception message: " + "\"" + exc.getMessage() + "\"");
             return FileVisitResult.CONTINUE;
         }
     }
--- java/ru/ifmo/rain/golovin/walk/RecursiveWalk.java
+++ java/ru/ifmo/rain/golovin/walk/RecursiveWalk.java
@@ -1,12 +1,14 @@
 package ru.ifmo.rain.golovin.walk;
 
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.FileInputStream;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
@@ -14,30 +16,49 @@ import java.nio.file.Paths;
 
 public class RecursiveWalk {
     public static void main(String[] args) {
-
-        if (args.length < 2) {
-            System.out.println("Too few args");
-        } else if (args.length > 2) {
-            System.out.println("Too many args");
-        } else {
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0]), "UTF8"))) {
-                try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(args[1]), "UTF8"))) {
-                    for (String pathname = reader.readLine(); pathname != null; pathname = reader.readLine()) {
-                        try {
-                            Path path = Paths.get(pathname);
-                            Files.walkFileTree(path, new FileVisitorHash(writer));
+        try {
+            if (args == null || args.length != 2 || args[0] == null || args[1] == null) {
+                System.out.println("Invalid input parameter.");
+            } else {
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0]), "UTF8"))) {
+                    try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(args[1]), "UTF8"))) {
+                        for (String pathname = reader.readLine(); pathname != null; pathname = reader.readLine()) {
+                            try {
+                                Path path = Paths.get(pathname);
+                                Files.walkFileTree(path, new FileVisitorHash(writer, path.toString()));
+                            } catch (InvalidPathException e) {
+                                writer.write(String.format("%08X", 0).toLowerCase() + " " + pathname);
+                                System.out.println("Invalid path exception during access to starting path for walking. message: "  + "\"" + e.getMessage() + "\"");
+                            } catch (SecurityException e) {
+                                writer.write(String.format("%08X", 0).toLowerCase() + " " + pathname);
+                                System.out.println("Security exception during access to starting path for walking. message: "  + "\"" + e.getMessage() + "\"");
+                            } catch (IOException e) {
+                                System.out.println("I/O exception during walk. message: " + "\"" + e.getMessage() + "\"");
+                            }
                         }
-                        catch (InvalidPathException e) {
-                            writer.write(String.format("%08X", 0).toLowerCase() + " " + pathname);
-                            System.out.println("Invalid path \"" + pathname + "\"");
-                        }
+                    } catch (FileNotFoundException e) {
+                        System.out.println("Output file not found. Caught file not found exception with message: " + "\"" + e.getMessage() + "\"");
+                    } catch (SecurityException e) {
+                        System.out.println("Security exception during open output file. message: " + "\"" + e.getMessage() + "\"");
+                    } catch (UnsupportedEncodingException e) {
+                        System.out.println("Output file is not supported charset \"UTF8\". Caught unsupported encoding exception with message: " + "\"" + e.getMessage() + "\"");
+                    } catch (IOException e) {
+                        System.out.println("I/O exception during close output file. message: " + "\"" + e.getMessage() + "\"");
                     }
-                } catch (IOException ignore) {
-                    System.out.println("Output file \""+ args[1] +"\" open error");
+                } catch (FileNotFoundException e) {
+                    System.out.println("Input file not found. Caught file not found exception with message: " + "\"" + e.getMessage() + "\"");
+                } catch (SecurityException e) {
+                    System.out.println("Security exception during open input file. message: " + "\"" + e.getMessage() + "\"");
+                } catch (UnsupportedEncodingException e) {
+                    System.out.println("Input file is not supported charset \"UTF8\". Caught unsupported encoding exception with message: " + "\"" + e.getMessage() + "\"");
+                } catch (IOException e) {
+                    System.out.println("I/O exception during close input file. message: " + "\"" + e.getMessage() + "\"");
                 }
-            } catch (IOException ignore) {
-                System.out.println("Input file \""+ args[0] + "\" open error");
             }
+        } catch (Error e) {
+            System.out.println("Fatal error: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println("Runtime exception: " + e.getMessage());
         }
     }
 }
