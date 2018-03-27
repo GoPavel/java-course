ecd0444d75ccaa63ccfca24f9c9f33d87086a615
Golovin Pavel <gopavel0@gmail.com>
2018-03-28 02:17:23 +0300

    HW 4 commit 2

--- java/ru/ifmo/rain/golovin/implementor/ImplTester.java
+++ java/ru/ifmo/rain/golovin/implementor/ImplTester.java
@@ -1,5 +1,10 @@
 package ru.ifmo.rain.golovin.implementor;
 
+import info.kgeorgiy.java.advanced.implementor.standard.basic.Accessible;
+import info.kgeorgiy.java.advanced.implementor.standard.full.BMPImageWriteParam;
+import info.kgeorgiy.java.advanced.implementor.standard.full.LdapReferralException;
+import info.kgeorgiy.java.advanced.implementor.standard.full.SDeprecated;
+
 import java.nio.file.Paths;
 
 public class ImplTester {
@@ -7,7 +12,7 @@ public class ImplTester {
 
     public static void main(String[] args) {
         try {
-            hero.implement(A.class, Paths.get("."));
+            hero.implement(SDeprecated.class, Paths.get("/media/pavel/DATA/java-course/java/ru/ifmo/rain/golovin/implementor/temp/"));
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
---
+++ java/ru/ifmo/rain/golovin/implementor/PavelTest.java
@@ -0,0 +1,4 @@
+package ru.ifmo.rain.golovin.implementor;
+
+public class PavelTest {
+}
