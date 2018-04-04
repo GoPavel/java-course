3689c1cad238494f47d211683e08e60563de4949
Golovin Pavel <gopavel0@gmail.com>
2018-04-04 13:30:50 +0300

    HW 7 commit 2

--- java/ru/ifmo/rain/golovin/concurrent/ListIPImpl.java
+++ java/ru/ifmo/rain/golovin/concurrent/ListIPImpl.java
@@ -1,8 +1,10 @@
-package ru.ifmo.rain.golovin.cuncurrent;
+package ru.ifmo.rain.golovin.concurrent;
 
 import info.kgeorgiy.java.advanced.concurrent.ListIP;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -18,12 +20,12 @@ public class ListIPImpl implements ListIP {
     static private class Folding<T, MapT> implements Runnable {
 
         private Stream<? extends T> values;
-        private Function<T, MapT> mapper;
+        private Function<? super T, MapT> mapper;
         private BinaryOperator<MapT> operator;
         private int indexForResult;
         private ArrayList<MapT> result;
 
-        Folding(List<? extends T> list, Function<T, MapT> map, BinaryOperator<MapT> op, ArrayList<MapT> res, int resIndex) {
+        Folding(List<? extends T> list, Function<? super T, MapT> map, BinaryOperator<MapT> op, ArrayList<MapT> res, int resIndex) {
             values = list.stream();
             mapper = map;
             operator = op;
@@ -37,21 +39,25 @@ public class ListIPImpl implements ListIP {
         }
     }
 
-    private <T, MapT> MapT runThread(int cntThreads, List<? extends T> values, Function<T, MapT> map, BinaryOperator<MapT> op)
+    private <T, MapT> MapT runThread(int cntThreads, List<? extends T> values, Function<? super T, MapT> map, BinaryOperator<MapT> op)
         throws  InterruptedException {
         int lenPart = (values.size() + cntThreads - 1) / cntThreads;
 
         Thread[] threads = new Thread[cntThreads];
+        int cntRunned = 0;
 
-        ArrayList<MapT> results = new ArrayList<>(cntThreads);
+        ArrayList<MapT> results = new ArrayList<>(Collections.nCopies(cntThreads, null));
 
-        for (int l = 0, r = lenPart, i = 0; l < values.size(); l = r, r += lenPart, ++i) {
-            threads[i] = new Thread(new Folding<T, MapT>(values.subList(l, r), map, op, results, i));
-            threads[i].start();
+        for (int l = 0, r = lenPart; l < values.size(); l = r, r += lenPart, cntRunned++) {
+            threads[cntRunned] = new Thread(new Folding<T, MapT>(values.subList(l, r > values.size() ? values.size() : r), map, op, results, cntRunned));
+            threads[cntRunned].start();
         }
 
-        for (int i = 0; i < cntThreads; ++i) {
+        for (int i = 0; i < cntRunned; ++i) {
             try {
+                if (threads[i] == null) {
+                    System.out.println("NULL " + i);
+                }
                 threads[i].join();
             } catch (InterruptedException e) {
                 throw e;
@@ -65,43 +71,56 @@ public class ListIPImpl implements ListIP {
     public <T> T
     maximum(int threads, List<? extends T> values, Comparator<? super T> comparator)
             throws InterruptedException {
-        return runThread(threads, values, o -> o, (o1, o2) -> (comparator.compare(o1, o2) >= 0) ? o1 : o2);
+        return runThread(threads, values, o -> o, (o1, o2) -> (Comparator.nullsFirst(comparator).compare(o1, o2) >= 0) ? o1 : o2);
     }
 
     @Override
     public <T> T
     minimum(int threads, List<? extends T> values, Comparator<? super T> comparator)
             throws InterruptedException {
-        return runThread(threads, values, o -> o, (o1, o2) -> (comparator.compare(o1, o2) <= 0) ? o1 : o2);
+        return runThread(threads, values, o -> o, (o1, o2) -> (Comparator.nullsLast(comparator).compare(o1, o2) <= 0) ? o1 : o2);
     }
 
     @Override
     public <T> boolean
     all(int threads, List<? extends T> values, Predicate<? super T> predicate)
             throws InterruptedException {
-        return runThread(threads, values, predicate, );
+        return this.<T, Boolean>runThread(threads, values, o -> (o != null ? predicate.test(o) : true ), (b1, b2) -> b1 && b2 );
     }
 
     @Override
     public <T> boolean
     any(int threads, List<? extends T> values, Predicate<? super T> predicate)
             throws InterruptedException {
-
+        return this.<T, Boolean>runThread(threads, values, o -> (o != null ? predicate.test(o) : false ), (b1, b2) -> b1 || b2);
     }
-
 
     @Override
     public String
     join(int threads, List<?> values)
-            throws InterruptedException { }
+            throws InterruptedException {
+        return runThread(threads, values, Object::toString, (s, s2) -> s + s2);
+    }
 
     @Override
     public <T> List<T>
     filter(final int threads, final List<? extends T> values, final Predicate<? super T> predicate)
-            throws InterruptedException { }
+            throws InterruptedException {
+        return runThread(threads, values, o -> predicate.test(o) ? Arrays.asList(o) : Collections.emptyList(),
+                (ts, ts2) -> {
+                    ts.addAll(ts2);
+                    return ts;
+                });
+    }
 
     @Override
     public <T, U> List<U>
     map(final int threads, final List<? extends T> values, final Function<? super T, ? extends U> f)
-            throws InterruptedException { }
+            throws InterruptedException {
+        return this.<T, List<U>>runThread(threads, values, o -> Arrays.asList(f.apply(o)),
+                (ts, ts2) -> {
+                    ts.addAll(ts2);
+                    return ts;
+                });
+    }
 }
--- java/ru/ifmo/rain/golovin/implementor/ImplTester.java
+++ java/ru/ifmo/rain/golovin/implementor/ImplTester.java
@@ -1,5 +1,6 @@
 package ru.ifmo.rain.golovin.implementor;
 
+import info.kgeorgiy.java.advanced.concurrent.ListIP;
 import info.kgeorgiy.java.advanced.implementor.standard.basic.Accessible;
 import info.kgeorgiy.java.advanced.implementor.standard.full.BMPImageWriteParam;
 import info.kgeorgiy.java.advanced.implementor.standard.full.LdapReferralException;
@@ -12,7 +13,7 @@ public class ImplTester {
 
     public static void main(String[] args) {
         try {
-            hero.implement(BMPImageWriteParam.class, Paths.get("/media/pavel/DATA/java-course/java/ru/ifmo/rain/golovin/implementor/temp/"));
+            hero.implement(ListIP.class, Paths.get("/media/pavel/DATA/java-course/java/ru/ifmo/rain/golovin/implementor/temp/"));
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
